914913c32cee67c9f6705a8b900ff8b7f9adbdb7
leoldding <leo.ld.ding@gmail.com>
2024-08-13 12:44:56 -0400

    created CRUD handlers and database

---
+++ backend/handlers/handlers.go
@@ -0,0 +1,58 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	db "github.com/leoldding/cpf-wl/database"
+)
+
+func RegisterHandlers(router *mux.Router, database *db.Database) {
+	log.Println("Registering Handlers")
+	router.HandleFunc("/api/users", createUser(database)).Methods("POST")
+	router.HandleFunc("/api/users", getUsers(database)).Methods("GET")
+	router.HandleFunc("/api/users", updateUser(database)).Methods("PATCH")
+	router.HandleFunc("/api/users/{id}", deleteUser(database)).Methods("DELETE")
+}
+
+func createUser(database *db.Database) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var newUser *db.User
+		if err := json.NewDecoder(r.Body).Decode(&newUser); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		database.AddUser(newUser)
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(newUser)
+	}
+}
+
+func getUsers(database *db.Database) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		users := database.GetUsers()
+		json.NewEncoder(w).Encode(users)
+	}
+}
+
+func updateUser(database *db.Database) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var user *db.User
+		if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		database.UpdateUser(user)
+		w.Write([]byte("Success"))
+	}
+}
+
+func deleteUser(database *db.Database) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := mux.Vars(r)["id"]
+		database.DeleteUser(id)
+		w.Write([]byte("Success"))
+	}
+}
--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	gorillaHandlers "github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	db "github.com/leoldding/cpf-wl/database"
+	"github.com/leoldding/cpf-wl/handlers"
+)
 
 func main() {
-	fmt.Println("Hello")
+	router := mux.NewRouter()
+	router.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	})
+
+	database := db.NewDatabase()
+	database.LoadMockData()
+
+	handlers.RegisterHandlers(router, database)
+
+	headersOk := gorillaHandlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type"})
+	originsOk := gorillaHandlers.AllowedOrigins([]string{"http://localhost:5173"})
+	methodsOk := gorillaHandlers.AllowedMethods([]string{"POST", "GET", "PATCH", "DELETE"})
+
+	log.Println("Server is running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", gorillaHandlers.CORS(originsOk, headersOk, methodsOk)(router)))
 }
