0b5c8765555122550790bf29833e4644d237613d
leoldding <leo.ld.ding@gmail.com>
2024-08-20 14:38:49 -0400

    added database connection with pgx

--- backend/database/database.go
+++ backend/database/database.go
@@ -1,14 +1,15 @@
 package database
 
 import (
+	"context"
 	"crypto/rand"
 	"encoding/hex"
 	"log"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-type Database struct {
-	Users []*User
-}
+var postgresConn = "postgresql://localhost:5432/postgres"
 
 type User struct {
 	Id        string
@@ -18,49 +19,111 @@ type User struct {
 	Total     int
 }
 
-func NewDatabase() *Database {
-	return &Database{[]*User{}}
+func NewDatabase(ctx context.Context) (*pgxpool.Pool, error) {
+	pool, err := pgxpool.New(ctx, postgresConn)
+	if err != nil {
+		log.Printf("Unable to create database pool: %v", err)
+		return nil, err
+	}
+	return pool, nil
 }
 
-func (database *Database) LoadMockData() {
+func LoadMockData(ctx context.Context, pool *pgxpool.Pool) error {
 	log.Println("LOADING MOCK DATA")
-	database.CreateUser(&User{Name: "Alfred Alvarado", Snatch: 20, CleanJerk: 40})
-	database.CreateUser(&User{Name: "Benjamin Bolognese", Snatch: 40, CleanJerk: 90})
-	database.CreateUser(&User{Name: "Charles Charleston", Snatch: 10, CleanJerk: 20})
-	database.CreateUser(&User{Name: "Damian Dog", Snatch: 150, CleanJerk: 290})
+	if err := CreateUser(ctx, pool, &User{Name: "Alfred Alvarado", Snatch: 20, CleanJerk: 40}); err != nil {
+		return err
+	}
+	if err := CreateUser(ctx, pool, &User{Name: "Benjamin Bolognese", Snatch: 40, CleanJerk: 90}); err != nil {
+		return err
+	}
+	if err := CreateUser(ctx, pool, &User{Name: "Charles Charleston", Snatch: 10, CleanJerk: 20}); err != nil {
+		return err
+	}
+	if err := CreateUser(ctx, pool, &User{Name: "Damian Dog", Snatch: 150, CleanJerk: 290}); err != nil {
+		return err
+	}
+	return nil
 }
 
-func (database *Database) CreateUser(user *User) {
-	log.Println("ADDING USER: " + user.Name)
-	b := make([]byte, 4)
-	rand.Read(b)
+func CreateUser(ctx context.Context, pool *pgxpool.Pool, user *User) error {
+	conn, err := pool.Acquire(ctx)
+	if err != nil {
+		return err
+	}
+	defer conn.Release()
+
+	b := make([]byte, 16)
+	if _, err := rand.Read(b); err != nil {
+		log.Printf("Failed to generate user ID: %v\n", err)
+		return err
+	}
 	user.Id = hex.EncodeToString(b)
 	user.Total = user.Snatch + user.CleanJerk
-	database.Users = append(database.Users, user)
+	_, err = conn.Exec(ctx, "INSERT INTO leaderboard (id, name, snatch, cleanjerk, total) VALUES ($1, $2, $3, $4, $5);", user.Id, user.Name, user.Snatch, user.CleanJerk, user.Total)
+	if err != nil {
+		log.Printf("Failed to insert user %s: %v", user.Id, err)
+		return err
+	}
+	log.Println("ADDING USER: " + user.Name + " " + user.Id)
+	return nil
 }
 
-func (database *Database) GetUsers() []*User {
+func GetUsers(ctx context.Context, pool *pgxpool.Pool) ([]*User, error) {
+	conn, err := pool.Acquire(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Release()
+
+	rows, err := conn.Query(ctx, "SELECT id, name, snatch, cleanjerk, total FROM leaderboard;")
+	if err != nil {
+		log.Printf("Failed to retrieve users: %v", err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	var users []*User
+	for rows.Next() {
+		var user User
+		err := rows.Scan(&user.Id, &user.Name, &user.Snatch, &user.CleanJerk, &user.Total)
+		if err != nil {
+			log.Printf("Failed to parse user: %v", err)
+			return nil, err
+		}
+		users = append(users, &user)
+	}
 	log.Println("RETURNING LIST OF USERS")
-	return database.Users
+	return users, nil
 }
 
-func (database *Database) UpdateUser(user *User) {
-	for i, dbUser := range database.Users {
-		if user.Id == dbUser.Id {
-			database.Users[i] = user
-			log.Println("UPDATING USER: " + user.Id)
-			return
-		}
+func UpdateUser(ctx context.Context, pool *pgxpool.Pool, user *User) error {
+	conn, err := pool.Acquire(ctx)
+	if err != nil {
+		return err
 	}
+	defer conn.Release()
+
+	_, err = conn.Exec(ctx, "UPDATE leaderboard SET name = $2, snatch = $3, cleanjerk = $4, total = $5 WHERE id = $1;", user.Id, user.Name, user.Snatch, user.CleanJerk, user.Total)
+	if err != nil {
+		log.Printf("Failed to update user %s: %v", user.Id, err)
+		return err
+	}
+	log.Println("UPDATING USER: " + user.Name + " " + user.Id)
+	return nil
 }
 
-func (database *Database) DeleteUser(id string) {
-	for i, user := range database.Users {
-		if user.Id == id {
-			database.Users[i] = database.Users[len(database.Users)-1]
-			database.Users = database.Users[:len(database.Users)-1]
-			log.Println("DELETING USER: " + user.Id)
-			return
-		}
+func DeleteUser(ctx context.Context, pool *pgxpool.Pool, id string) error {
+	conn, err := pool.Acquire(ctx)
+	if err != nil {
+		return err
 	}
+	defer conn.Release()
+
+	_, err = conn.Exec(ctx, "DELETE FROM leaderboard WHERE id = $1;", id)
+	if err != nil {
+		log.Printf("Failed to delete user %s, :%v", id, err)
+		return err
+	}
+	log.Println("DELETING USER: " + id)
+	return nil
 }
